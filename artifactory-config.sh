ce70d2680194398a024207a7886898c99c4edcac
David Pinhas <45331381+davidpinhas@users.noreply.github.com>
2022-03-07 10:52:53 +0200

    Update artifactory-config.sh

--- artifactory-config.sh
+++ artifactory-config.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ -d "~/.jfrog/jfrog-cli.conf.v5" ]; then rm -Rf ~/.jfrog/jfrog-cli.conf.v5; fi
 jfrog config add davidp --artifactory-url https://davidp.jfrog.io/artifactory --user $int_pegeneric_user --password $int_pegeneric_password --interactive=false
 jfrog config use davidp
